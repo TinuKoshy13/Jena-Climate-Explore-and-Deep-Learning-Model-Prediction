6d8a3cf8f3687d20787961c09f1c5def00bcdf09
TinuKoshy13 <100341413+TinuKoshy13@users.noreply.github.com>
2023-01-12 19:31:53 +0100

    Update dlapp.py

--- dlapp.py
+++ dlapp.py
@@ -27,11 +27,6 @@ from streamlit_pandas_profiling import st_profile_report
 import plotly.express as px
 import streamlit as st
 import base64
-import pyautogui
-try:
-    st.success("Loading APP")
-except:
-   pyautogui.hotkey("ctrl","F5")
 
 def pd_prof(df):
   profile = ProfileReport(df_climate_hour, title="EDA Jena Climate Dataset")
